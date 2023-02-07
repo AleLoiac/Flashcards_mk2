4adff2c50c6e06eb8fbfacefa29c0c0cb89bc30e
AleLoiac <alessandroloiac@gmail.com>
2023-02-07 15:14:00 +0100

    07/02

--- Flashcards_mk2.go
+++ Flashcards_mk2.go
@@ -1,5 +1,46 @@
 package main
 
+import "fmt"
+
+type flashcard struct {
+	term       string
+	definition string
+}
+
+var flashcardDeck []flashcard
+
+func createCard() {
+
+}
+
 func main() {
+
+	for {
+		fmt.Println("Input the action (add, remove, import, export, ask, exit):")
+
+		var command string
+		_, err := fmt.Scan(&command)
+		if err != nil {
+			return
+		}
+
+		switch command {
+		case "add":
+			createCard()
+		case "remove":
+
+		case "import":
+
+		case "export":
+
+		case "ask":
+
+		case "exit":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Invalid command, please try again")
+		}
+	}
 
 }
